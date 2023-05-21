e4b14e1cc79a9351cf243f6862d52605f334d7f5
Sorskoot <mental_c@hotmail.com>
2023-05-21 13:53:50 +0200

    Start of growth cycle of plants

--- Assets/_game/Scripts/GameController.cs
+++ Assets/_game/Scripts/GameController.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
+
+public class GameState
+{
+    public IReadOnlyReactiveProperty<int> CurrentDay => currentDay;
+    private readonly IReactiveProperty<int> currentDay = new ReactiveProperty<int>(0);
+
+    public void SetDay(int day)
+    {
+        currentDay.Value = day;
+    }
+
+    public void AdvanceDay()
+    {
+        currentDay.Value++;
+    } 
+
+}
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    [Tooltip("The length of a day in seconds")]
+    private int lengthOfDay = 10;
+
+    public GameState State = new GameState();
+
     void Start()
     {
+        State.SetDay(0);
         
+        //Scheduler.Immediate.Schedule(TimeSpan.FromSeconds(10),() =>
+        //{
+        //    State.AdvanceDay();
+        //});
     }
 
-    // Update is called once per frame
+    float timer = 0;
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > lengthOfDay)
+        {
+            State.AdvanceDay();
+            timer = 0;
+        }
     }
 }
--- Assets/_game/Scripts/Plant.cs
+++ Assets/_game/Scripts/Plant.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
+
+[RequireComponent(typeof(SpriteRenderer))]
 public class Plant : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int GrowthStage = 0;
+
+    [SerializeField] private Sprite[] GrowthSprites;
+
+    [SerializeField] private Sprite Seed;
+
+    [SerializeField] private Sprite Harvest;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
     {
-        
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+
+        var gameController = FindObjectOfType<GameController>();
+        gameController.State.CurrentDay.Subscribe(OnNewDay).AddTo(this);
+
+        GrowthStage = 0;
+        spriteRenderer.sprite = GrowthSprites[this.GrowthStage];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnNewDay(int day)
     {
-        
+        Debug.Log(day);
+        this.Grow();
+    }
+
+    private void Grow()
+    {
+        if (this.GrowthStage < this.GrowthSprites.Length)
+        {
+            this.GrowthStage++;
+            spriteRenderer.sprite = GrowthSprites[Mathf.Min(this.GrowthStage, this.GrowthSprites.Length-1)];
+        }
     }
 }
