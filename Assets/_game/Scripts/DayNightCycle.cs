8006f19156010cd8b7d7582664f47e40b90a2a1a
Sorskoot <mental_c@hotmail.com>
2023-05-26 18:16:33 +0200

    WIP

---
+++ Assets/_game/Scripts/DayNightCycle.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private float dayDuration = 30f; // Duration of a full day cycle (day + night) in seconds
+    [SerializeField] private Material skyboxMaterial;
+
+    private Light sun;
+    private float rotationSpeed;
+
+    void Start()
+    {
+        sun = GetComponent<Light>();
+        rotationSpeed = 360f / dayDuration;
+
+        if (skyboxMaterial != null)
+        {
+            RenderSettings.skybox = skyboxMaterial;
+            
+        }
+    }
+
+    void Update()
+    {
+        // Rotate the sun based on time elapsed
+        float angle = Time.deltaTime * rotationSpeed;
+        sun.transform.Rotate(new Vector3(angle, 0, 0));
+
+        // Adjust intensity for nighttime
+        float currentSunElevation = sun.transform.eulerAngles.x;
+
+        // Adjust intensity for nighttime and update Skybox exposure
+        if (currentSunElevation >= 180)
+        {
+            // Nighttime: reduce intensity so that it's dark but still visible.
+            sun.intensity = Mathf.Lerp(sun.intensity, 0.1f, Time.deltaTime);
+
+            // Set Skybox exposure to 0.25 during nighttime
+            skyboxMaterial.SetFloat("_Exposure",
+                Mathf.Lerp(skyboxMaterial.GetFloat("_Exposure"), 0.25f, Time.deltaTime));
+            RenderSettings.ambientIntensity = Mathf.Lerp(RenderSettings.ambientIntensity, 0.7f, Time.deltaTime);
+        }
+        else
+        {
+            // Daytime: restore the original intensity.
+            sun.intensity = Mathf.Lerp(sun.intensity, 1f, Time.deltaTime);
+            RenderSettings.ambientIntensity = Mathf.Lerp(RenderSettings.ambientIntensity, 1f, Time.deltaTime);
+            // Set Skybox exposure to 1 during daytime
+            skyboxMaterial.SetFloat("_Exposure", Mathf.Lerp(skyboxMaterial.GetFloat("_Exposure"), 1f, Time.deltaTime));
+        }
+    }
+}
