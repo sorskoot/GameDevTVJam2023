8560c3aadc869ebde0aa80550cc80379b9c80543
Sorskoot <mental_c@hotmail.com>
2023-05-29 20:47:21 +0200

    Selecting Stuff

--- Assets/_game/Scripts/CropGrowthArea.cs
+++ Assets/_game/Scripts/CropGrowthArea.cs
@@ -25,15 +25,27 @@ public class CropGrowthArea : MonoBehaviour
 
     [SerializeField] private Sprite wetDirt;
 
+    [SerializeField] private GameObject canPlace;
+    [SerializeField] private GameObject cannotPlace;
+    [SerializeField] private GameObject highLightCrop;
+
+    private bool isHovering = false;
+
     // Start is called before the first frame update
     void Start()
     {
         hasPlant = false;
+        canPlace.SetActive(false);
+        cannotPlace.SetActive(false);
+        highLightCrop.SetActive(false);
+
         gameState = FindObjectOfType<GameController>().State;
         growthAreaState = new GrowthAreaState(gameState);
         soil.sprite = grass;
 
         growthAreaState.CurrentSoil.Subscribe(OnSoilChanged).AddTo(this);
+
+
     }
 
     private void OnSoilChanged(Soil soil)
@@ -90,6 +102,7 @@ public class CropGrowthArea : MonoBehaviour
                 FindObjectOfType<SFXController>().PlayWateringCan();
                 var splash = Instantiate(SplashEffect, transform.position, Quaternion.identity);
                 splash.transform.Rotate(0, 90, 0);
+                OnHoverExit();
                 return;
             }
         }
@@ -135,11 +148,64 @@ public class CropGrowthArea : MonoBehaviour
                 gameState.SubtractMoney(selectedPlant.Price);
                 FindObjectOfType<SFXController>().PlayPlant();
                 plant = selectedPlant;
+                OnHoverExit();
                 return;
             }
 
             FindObjectOfType<SFXController>().PlayFailSound();
         }
+    }
+
+    public void OnHoverEnter()
+    {
+        isHovering = true;
+
+        if (!hasPlant)
+        {
+            if (gameState.CanPlantSelected() && growthAreaState.CurrentSoil.Value == Soil.Wet)
+            {
+                var selectedPlant = gameState.SelectedPlant.Value.GetComponent<Plant>();
+                var sprite = selectedPlant.HarvestSprite;
+                highLightCrop.GetComponent<SpriteRenderer>().sprite = sprite;
+                highLightCrop.SetActive(true);
+                return;
+            }
+            if (gameState.SelectedTool.Value == Tool.Hoe && growthAreaState.CurrentSoil.Value == Soil.Grass)
+            {
+                canPlace.SetActive(true);
+                return;
+            }
+        }
+        else
+        {
+            if (plant.IsHarvestable())
+            {
+                canPlace.SetActive(true);
+                return;
+            }
+            if (plant.IsDead() && gameState.SelectedTool.Value == Tool.Rake)
+            {
+                canPlace.SetActive(true);
+                return;
+            }
+
+        }
+
+        if (gameState.SelectedTool.Value == Tool.WateringCan && growthAreaState.CurrentSoil.Value == Soil.Dirt)
+        {
+            canPlace.SetActive(true);
+            return;
+        }
+
+        cannotPlace.SetActive(true);
+    }
+
+    public void OnHoverExit()
+    {
+        canPlace.SetActive(false);
+        cannotPlace.SetActive(false);
+        highLightCrop.SetActive(false);
+        isHovering = false;
     }
 
     private void Dispose()
